9e6bedd26b286e4d893a3a1dcd1560871db102d6
vivek mishra <vm368@afsconnect1.njit.edu>
2017-10-21 04:03:11 -0400

    upload final

---
+++ upload.php
@@ -0,0 +1,24 @@
+<?php
+
+class CsvUpload {
+
+    function upload_csv() {
+        if ($_FILES['csv']['error'] == 0) {
+            $name = $_FILES['csv']['name'];
+            $ext = strtolower(end(explode('.', $name)));
+            $type = $_FILES['csv']['type'];
+            if ($ext === 'csv') {
+                move_uploaded_file($_FILES['csv']['tmp_name'], 'uploads/' . $name);
+                header('Location: index.php?filename=' . urldecode($name));
+            } else {
+                echo 'Invalid file. Please upload valid csv file..';
+            }
+        }
+    }
+
+}
+
+$upd = new CsvUpload();
+if (isset($_GET['upload_csv'])) {
+    $upd->upload_csv();
+}
